42c9ed2b458c21aa444ebffff7672593f250ebba
milin <milindshandilya899@gmail.com>
2026-05-04 12:30:37 +0530

    Fix all 3 activities: npm install for audit, lightweight=false for job init, groovy XML job creation

---
+++ init.groovy.d/01-credentials.groovy
@@ -0,0 +1,34 @@
+import jenkins.model.*
+import hudson.util.Secret
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.*
+import org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl
+import org.apache.commons.fileupload.FileItem
+import hudson.FilePath
+import java.nio.file.Files
+
+def jenkins = Jenkins.get()
+
+def kubeFile = new File('/var/jenkins_casc/jenkins-kubeconfig')
+if (kubeFile.exists()) {
+    def store = jenkins.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+    def domain = Domain.global()
+
+    def existing = store.getCredentials(domain).find { it.id == 'k8s-kubeconfig' }
+    if (!existing) {
+        def secretBytes = SecretBytes.fromBytes(kubeFile.bytes)
+        def cred = new FileCredentialsImpl(
+            CredentialsScope.GLOBAL,
+            'k8s-kubeconfig',
+            'Kubernetes kubeconfig',
+            'kubeconfig',
+            secretBytes
+        )
+        store.addCredentials(domain, cred)
+        println 'Created k8s-kubeconfig credential'
+    } else {
+        println 'k8s-kubeconfig credential already exists'
+    }
+}
+
+jenkins.save()
